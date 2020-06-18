3ba20ef7c397faca6b44253471e64e177b7460dc
Pescaru Alexandru <pescarualexflorin@gmail.com>
2020-06-18 21:31:40 +0300

    studied arrays and classic for loop

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -4,17 +4,52 @@ import java.util.Scanner;
 
 public class Game {
 
+    private Track[] tracks = new Track[3];
+
+
+
     public  void start(){
         System.out.println("Welcome to the racing game!");
 
        int playerCount = getPlayerCountFromUsers();
         System.out.println("Pleyer count : " + playerCount);
+
+        initializeTraks();
     }
+
+
+
 
     private int getPlayerCountFromUsers(){
         System.out.println("Please enter number of players: ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
 
+    }
+
+    private void initializeTraks() {
+        Track track1 = new Track();
+        track1.setName("Street Circuit");
+        track1.setLenght(300);
+
+        tracks[0] = track1;
+
+        Track track2 = new Track();
+        track2.setName("Forest Circuit");
+        track2.setLenght(100);
+
+        tracks[1] = track2;
+
+        displayTraks();
+    }
+
+    private void displayTraks() {
+        System.out.println("Available traks: ");
+
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] != null) {
+                System.out.println(i + 1 + ". " + tracks[i].getName() + ": " + tracks[i].getLenght() + " km");
+            }
+        }
     }
 }
---
+++ src/main/java/org/fasttrackit/Track.java
@@ -0,0 +1,34 @@
+package org.fasttrackit;
+
+public class Track {
+
+    private String name;
+    private int lenght;
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "name='" + name + '\'' +
+                ", lenght=" + lenght +
+                '}';
+    }
+}
